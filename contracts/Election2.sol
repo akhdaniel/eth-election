cc37dc745300bc71372ee91379d34a541a1bb9fc
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-07 06:35:26 +0700

    vote atas nama system account

--- contracts/Election2.sol
+++ contracts/Election2.sol
@@ -1,10 +1,10 @@
-pragma solidity 0.4 .20;
+pragma solidity 0.4.20;
 
-contract Election {
-    // Model a Candidate
+contract Election2 {
     /*
-    vote atas nama masing2 account
+    vote atas nama system account
     */
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
@@ -12,7 +12,8 @@ contract Election {
     }
 
     // Store accounts that have voted
-    mapping(address => bool) public voters;
+    // mapping(address => bool) public voters;
+    mapping(uint => bool) public voters;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -20,32 +21,33 @@ contract Election {
     uint public candidatesCount;
 
     // voted event
-    event votedEvent(
+    event votedEvent (
         uint indexed _candidateId
     );
 
-    function Election() public {
+    function Election2 () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
-    function addCandidate(string _name) public {
-        candidatesCount++;
+    function addCandidate (string _name) public {
+        candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote(uint _candidateId) public {
+    function vote (uint _candidateId, uint voterId) public {
         // require that they haven't voted before
-        require(!voters[msg.sender]);
+        // require(!voters[msg.sender]);
+        require(!voters[voterId]);
 
         // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
         // record that voter has voted
-        voters[msg.sender] = true;
+        voters[voterId] = true;
 
         // update candidate vote Count
-        candidates[_candidateId].voteCount++;
+        candidates[_candidateId].voteCount ++;
 
         // trigger voted event
         votedEvent(_candidateId);
