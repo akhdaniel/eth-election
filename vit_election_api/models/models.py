3216f90bbf633db28802def25c8576c5261eccd4
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-10 11:10:13 +0700

    Update models.py

--- vit_election_api/models/models.py
+++ vit_election_api/models/models.py
@@ -13,27 +13,27 @@ class res_company(models.Model):
     def bsc_connect(self):
 
         ICP = self.env['ir.config_parameter'].sudo()
-        ABI = ICP.get_param('vit_election.abi')
-        BSC = ICP.get_param('vit_election.bsc_url')
-        CONTRACT_ADDRESS=ICP.get_param('vit_election.contract_address')
-        ACCOUNT_ADDRESS_1=ICP.get_param('vit_election.system_address')
-        PRIVATE_KEY_1=ICP.get_param('vit_election.system_private_key')
-        CHAIN_ID=ICP.get_param('vit_election.chain_id')
+        abi = ICP.get_param('vit_election.abi')
+        bsc = ICP.get_param('vit_election.bsc_url')
+        contract_address = ICP.get_param('vit_election.contract_address')
+        account_address_1 = ICP.get_param('vit_election.system_address')
+        private_key_1 = ICP.get_param('vit_election.system_private_key')
+        chain_id = int(ICP.get_param('vit_election.chain_id'))
 
-        web3 = Web3(Web3.HTTPProvider(BSC))
-        contract_address = web3.toChecksumAddress(CONTRACT_ADDRESS)
-        contract = web3.eth.contract(address=contract_address, abi=ABI)   
-        system_account = web3.toChecksumAddress(ACCOUNT_ADDRESS_1)
-        system_private_key = PRIVATE_KEY_1
+        web3 = Web3(Web3.HTTPProvider(bsc))
+        contract_address = web3.toChecksumAddress(contract_address)
+        contract = web3.eth.contract(address=contract_address, abi=abi)   
+        system_account = web3.toChecksumAddress(account_address_1)
+        system_private_key = private_key_1
 
-        res = "BSC connected: {}\n".format( web3.isConnected() )
+        res = "bsc connected: {}\n".format( web3.isConnected() )
         res += 'candidatesCount={}\n'.format( contract.functions.candidatesCount().call())
         res += 'candidates(1)={}\n'.format(contract.functions.candidates(1).call())
         res += 'nonce={}\n'.format( web3.eth.get_transaction_count(system_account))
         res += 'gasPrice={}\n'.format(web3.eth.gas_price)
         _logger.info('bsc_test_connection: %s', res)
 
-        return web3,contract,system_account,system_private_key,CHAIN_ID
+        return web3,contract,system_account,system_private_key,chain_id
 
     def bsc_add_candidate(self, candidate_name, voting_session_id):
         try:
