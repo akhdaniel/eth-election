3b83d2b6bea0fbb7d14a53f28bee5bde3414c2a9
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-10 10:37:10 +0700

    Update voter.py

--- vit_election_app_inherit/model/voter.py
+++ vit_election_app_inherit/model/voter.py
@@ -19,11 +19,15 @@ class voter(models.Model):
     def action_add_voter(self):
         res = self.env['res.company'].bsc_add_voter(self.name)
         _logger.info('res = %s', res)
+        if res['status'] == -1:
+            raise UserError(res['message'])
         self.address = res['address']
         self.rx_receipt = res['rx_receipt']
 
     def action_add_candidate(self):
         res = self.env['res.company'].bsc_add_candidate(self.name, self.voting_session_id.id)
         _logger.info('res = %s', res)
+        if res['status'] == -1:
+            raise UserError(res['message'])
         self.rx_receipt = res['rx_receipt']
         # self.address = res.address
